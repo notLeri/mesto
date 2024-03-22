eba545ab15b5537fd5d3c6ac3d35c9355e916c1b
notLeri <ed.nazarov001@gmail.com>
2024-03-22 17:36:43 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const listOfCards = document.querySelector('.places__list');
+const addCardButton = document.querySelector('.profile__add-button');
+
+// @todo: Функция создания карточкиx
+
+function createCard(name, source, deleteCard) {
+    const cardClone = cardTemplate.querySelector('.card').cloneNode(true);
+
+    const cardButtonDelete = cardClone.querySelector('.card__delete-button');
+    const cardImgSrc = cardClone.querySelector('.card__image');
+    const cardTitle = cardClone.querySelector('.card__title');
+
+    cardTitle.textContent = name;
+    cardImgSrc.setAttribute('src', source);
+
+    cardButtonDelete.addEventListener('click', deleteCard(cardClone));
+    
+    console.log(cardClone);
+    return cardClone;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(card) {
+    return () => card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (card) {
+    const modifiedCard = createCard(card.name, card.link, deleteCard);
+    listOfCards.append(modifiedCard); 
+});
+
+
